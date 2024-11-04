656443039268d40ab4a2843a59e0e4b8d0419e40
Vlad Ponomarev <ponomarev.educ@gmail.com>
2024-11-04 22:06:07 +0000

    removed unused plugins from neovim config

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -4,7 +4,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "cpp" ,"c", "python", "lua", "vim", "vimdoc", "query",
-                        "java"},
+                        "java", "javascript"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -15,7 +15,6 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     -- 'false' will disable the whole extension
     enable = true,
-	
     additional_vim_regex_highlighting = false,
     },
 }
